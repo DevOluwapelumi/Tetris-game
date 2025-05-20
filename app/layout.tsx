a465179810f04d903642f705dcfc555ff9533366
Victor Oluwapelumi <victoroluwadarasimi16@gmail.com>
2025-05-20 09:25:41 +0100

    formatting commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,12 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
+  title: 'Tetris',
   description: 'Created with v0',
   generator: 'v0.dev',
+   icons: {
+    icon: '/Tetris icon.jpg', // path to your favicon file
+  },
 }
 
 export default function RootLayout({
